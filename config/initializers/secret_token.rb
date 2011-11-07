3b39b9170bd9c9112755342524ebb88ae1fe0035
Christian Szardenings <chris@szar.de>
2011-11-07 13:47:48 +0100

    Added Rails 3.1.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YouPM::Application.config.secret_token = 'e3dc5e52361cc3d245d67ba2c0486db6ed4968c3b20455d62a78c0da55ea585fd32c668fefc33d37c036fa7f4a2f868e0603c7095290197eea1c3a96cd168f41'
