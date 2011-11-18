c156c1099e56f3f4e5ddb9d6c8e774ce85068bdc
George Kuan <gkuan@exmoor.local>
2011-11-18 06:02:33 -0800

    Adding scaffolds for models. Still need to add the relationships between them. Renaming VendorAssignment WorkAssignment to better map to Rails nomenclature.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,37 @@
 YouPM::Application.routes.draw do
+  resources :work_assignments
+
+  resources :vendors
+
+  resources :property_assets
+
+  resources :property_units
+
+  resources :managers
+
   get "index/index"
 
   root to: 'index#index'
 
-  # Test routes for showing bootstrap design suggesstions
+  # Test routes for showing bootstrap design suggestions
+  get "signup" => "managers#signup"
+  get "confirm" => "managers#confirmemail"
 
+  get "portfolio" => "managers#portfolio"
 
+  resources :properties
+
+  resources :ads
+  
+  get "repost" => "ads#repost"
+  
+  resources :activities
+  
+  resources :tenants
+
+  resources :bills
+
+  resources :workorders
+
+  get "completedorder" => "workorders#markcomplete"
 end
